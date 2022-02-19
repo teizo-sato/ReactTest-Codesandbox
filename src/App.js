b8ef99afa2d562e9a9dcada5e9a7d97a2e54fd4a
teizo-sato <97894056+teizo-sato@users.noreply.github.com>
2022-02-19 20:49:56 +0900

    最後の修正

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "さとう",
-  image: "https://source.unsplash.com/Mv9hjnEUHR4",
-  email: "teaslkj-@gmai.com",
-  phone: "00-0000-000",
-  company: {
-    name: "コーポレーション名"
-  },
-  website: "http://www.teixofjkjj.com"
-};
-
 export default function App() {
   return <Router />;
 }
